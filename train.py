d61e15ec9cb525901cbbabfb033aa3e77591b5f4
Amit Aflalo <amit.aflalo2@gmail.com>
2024-07-15 14:44:49 +0300

    prot embd train

--- train.py
+++ train.py
@@ -15,22 +15,7 @@ from torch.optim import Adam
 from tqdm import tqdm
 import pickle
 
-def training(hyper_params, loader, epochs, device):
-
-
-    mol_encoder = TransformerEncoder(len(tokenizer.get_vocab()), embedding_dim=hyper_params['embedding_dim'],
-                                        hidden_size=hyper_params['embedding_dim'], nhead=hyper_params['encoder_n_head'],
-                                        n_layers=hyper_params['encoder_n_layer'],
-                                        max_length=hyper_params['max_mol_len'],
-                                        pad_token=tokenizer.token_to_id('<pad>'))
-    mol_decoder = TransformerDecoder(len(tokenizer.get_vocab()), embedding_dim=hyper_params['embedding_dim'],
-                                        hidden_size=hyper_params['embedding_dim'], nhead=hyper_params['decoder_n_head'],
-                                        n_layers=hyper_params['decoder_n_layer'], max_length=hyper_params['max_mol_len'])
-
-
-    model = InteractionTranslator(prot_encoder=InteractionEncoder(2 + 1024, hyper_params['embedding_dim']),
-                                    mol_encoder=mol_encoder, decoder=mol_decoder)
-    model.to(device)
+def training(model, tokenizer, hyper_params, loader, test_ds, epochs, device):
 
     print(f'model has: {sum(p.numel() for p in model.parameters() if p.requires_grad)} parameters')
     
@@ -67,17 +52,19 @@ def training(hyper_params, loader, epochs, device):
             optimizer.step()
             recon_losses.append(recon_loss)
 
-    validation_step(model, test_ds, 5, device)
+    validation_step(model, tokenizer, hyper_params, test_ds, 5, device)
 
-def validation_step(model, dataset, epoch, device):
-    print("*"  * 20 + "model saved" + "*" * 20)
+def validation_step(model, tokenizer, hyper_params, dataset, epoch, device):
     # save model for current validation
+    cur_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
     output_dir = Path(f'./models/{cur_time}/epoch{epoch + 1}')
     output_dir.mkdir(parents=True, exist_ok=True)
     torch.save(copy.deepcopy(model.state_dict()), f'{str(output_dir)}/model.pt')
     with open(f'{str(output_dir)}/hyper_params.json', 'w') as f:
         json.dump(hyper_params, f, indent=4)
     tokenizer.save(f'{str(output_dir)}/tokenizer_object.json')
+    
+    print("*"  * 20 + "model saved" + "*" * 20)
 
     model.eval()
     with torch.no_grad():
@@ -135,9 +122,8 @@ def validation_step(model, dataset, epoch, device):
             print(f'sim: {avg_similarity}, {std_similarity}')
             print(f'validity: {validity}')
 
-if __name__ == "__main__":
+def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    cur_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
     hyper_params = {
         'bs': 256,
         'lr': 0.0001,
@@ -162,8 +148,11 @@ if __name__ == "__main__":
     }
     print(hyper_params)
 
-    train_prot_embeds = np.load('./data/fsmol/prot_embeds_fsmol_train.npz')
-    valid_prot_embeds = np.load('./data/fsmol/prot_embeds_fsmol_valid.npz')
+    # train_prot_embeds = np.load('./data/fsmol/prot_embeds_fsmol_train.npz')
+    with open('prot_emb_train.pkl', 'rb') as file:
+        train_prot_embeds = pickle.load(file)
+    
+    # valid_prot_embeds = np.load('./data/fsmol/prot_embeds_fsmol_valid.npz')
     with open('prot_emb.pkl', 'rb') as file:
         test_prot_embeds = pickle.load(file)
 
@@ -177,9 +166,6 @@ if __name__ == "__main__":
     tokenizer = create_smiles_tokenizer(smiles, max_num_chains=25)
     tokenizer.enable_padding(pad_token="<pad>", length=hyper_params['max_mol_len'])
     
-    # model_path = "/Users/amitaflalo/Desktop/cfom/models/2023_05_24_14_46_54/epoch20"
-    # tokenizer = load_tokenizer_from_file(f'{model_path}/tokenizer_object.json')
-
     train_ds = InteractionsDataset(train_non_chiral_smiles, train_backbones, train_chains, train_assay_ids,
                                 train_types, train_labels, train_prot_embeds, tokenizer)
     
@@ -199,10 +185,15 @@ if __name__ == "__main__":
                                                             n_layers=hyper_params['decoder_n_layer'],
                                                             max_length=hyper_params['max_mol_len']))
     
-    model_path = "./models/2023_05_24_14_46_54/epoch20"
+    model_path = "./models/my_model"
     # model.load_state_dict(torch.load(f'{model_path}/model.pt', map_location=device))
+    # tokenizer = load_tokenizer_from_file(f'{model_path}/tokenizer_object.json')
+
     model.to(device)
 
     train_loader = DataLoader(train_ds, batch_size=hyper_params['bs'], shuffle=True)
     
-    training(hyper_params, train_loader, 20 ,device)
+    training(model, tokenizer, hyper_params, train_loader, test_ds, 20,device)
+
+if __name__ == "__main__":
+    main()
